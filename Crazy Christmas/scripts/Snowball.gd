653fa9a1acfd69fc3bb974dbcad7aba67656ab99
Ian Whitesel <altocgamedev@gmail.com>
2022-12-28 12:21:17 -0500

    exploding snowballs

--- Crazy Christmas/scripts/Snowball.gd
+++ Crazy Christmas/scripts/Snowball.gd
@@ -1,15 +1,28 @@
 extends RigidBody
 
-onready var deathTime = 3
+onready var deathFlag = false
+onready var explodeFlag = false
+onready var deathTime = 30
 onready var deathTimer = 0
 
+onready var mesh = get_node("MeshInstance")
+onready var particles = get_node("Particles")
+
 func _process(delta):
-	deathTimer += delta
-	if(deathTimer >= deathTime):
-		die()
+	if(deathFlag):
+		deathTimer += delta
+		if(deathTimer >= deathTime):
+			die()
+
+func _on_Snowball_body_entered(_body):
+	deathFlag = true
+	if(!explodeFlag):
+		explode()
+
+func explode():
+	mesh.visible = false
+	particles.emitting = true
 
 func die():
 	queue_free()
 
-func _on_Snowball_body_entered(_body):
-	die()
--- Crazy Christmas/scripts/TrapPit.gd
+++ Crazy Christmas/scripts/TrapPit.gd
@@ -6,22 +6,30 @@ onready var GLOBALS = get_node("/root/Main/Globals")
 onready var SIGNAL_BUS = get_node("/root/Main/SignalBus")
 
 onready var shrinkPlayer = false
-onready var player = get_node("/root/Main/Game/Player")
-onready var playerShrinkFactor = 0
-const shrinkPlayerMin = Vector3(0.01, 0.01, 0.01)
-const shrinkPlayerNorm = Vector3.ONE
+onready var shrinkOther = false
+onready var bodyToShrink = null
+onready var shrinkFactor = 0
+const shrinkMin = Vector3(0.01, 0.01, 0.01)
+const shrinkNorm = Vector3.ONE
 
 func _on_Area_body_entered(body):
+	if(body.is_in_group("snowball")):
+		shrinkOther = true
+		bodyToShrink = body
 	if(body.is_in_group("player")):
 		shrinkPlayer = true
+		bodyToShrink = body
 
 func _process(delta):
-	if(shrinkPlayer):
-		playerShrinkFactor += delta * 2
-		player.scale = shrinkPlayerNorm + (shrinkPlayerMin - shrinkPlayerNorm) * playerShrinkFactor
-		if(player.scale <= shrinkPlayerMin):
-			player.scale = shrinkPlayerNorm
-			playerShrinkFactor = 0
-			SIGNAL_BUS.emit_signal("playerTeleportIn")
-			SIGNAL_BUS.emit_signal("playerMoveToSpawn", GLOBALS.CURRENT_LEVEL.getPlayerSpawnCoords())
-			shrinkPlayer = false
+	if(shrinkPlayer || shrinkOther):
+		shrinkFactor += delta * 2
+		bodyToShrink.scale = shrinkNorm + (shrinkMin - shrinkNorm) * shrinkFactor
+		if(bodyToShrink.scale <= shrinkMin):
+			bodyToShrink.scale = shrinkNorm
+			shrinkFactor = 0
+			if(shrinkPlayer):
+				SIGNAL_BUS.emit_signal("playerTeleportIn")
+				SIGNAL_BUS.emit_signal("playerMoveToSpawn", GLOBALS.CURRENT_LEVEL.getPlayerSpawnCoords())
+				shrinkPlayer = false
+			else:
+				shrinkOther = false
