9e9167c7dabb8829106614373f0e861e0e8bf581
Ian Whitesel <altocgamedev@gmail.com>
2022-12-28 12:54:45 -0500

    new end game teleporter

--- Crazy Christmas/scripts/TeleportOut.gd
+++ Crazy Christmas/scripts/TeleportOut.gd
@@ -1,10 +1,21 @@
 extends Spatial
 
+onready var endGameTimer = 0
+const endGameTime = 0.5
+onready var endGameFlag = false
+
 ###REFS
 onready var MAIN = get_node("/root/Main")
 onready var GLOBALS = get_node("/root/Main/Globals")
 onready var SIGNAL_BUS = get_node("/root/Main/SignalBus")
 
+func _process(delta):
+	if(endGameFlag):
+		endGameTimer += delta
+		if(endGameTimer >= endGameTime):
+			MAIN.endGame()
+			endGameTimer = -99999
+
 func _on_Area_body_entered(body):
 	if(body.is_in_group("player")):
-		MAIN.endGame()
+		endGameFlag = true
